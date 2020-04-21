8600d9156e1f7bc463fa54c41179592c8be14333
rohit3345ks <rohit3345kumar@outlook.com>
2020-04-21 09:20:38 +0530

    Added Increase Insights and Changes since last count

--- app/views.py
+++ app/views.py
@@ -13,16 +13,27 @@ def home(request):
     res2=requests.get(daily_url).json()
     res3=requests.get(countrylist_url).json()['countries']
     reportDate=[]
-    confirmed_total=[]
+    confirmed_total=[0]
+    increase_confirmed=[]
+    # confirmed_total.append(0)
     country_list=[]
-    deaths_total=[]
+    deaths_total=[0]
+    # deaths_total.append(0)
+    recovered_total=[0]
+    recovered_total.append(res['recovered']['value'])
+    increase_recovered=[]
+    increase_recovered.append(recovered_total[-1]-recovered_total[-2])
+    increase_deaths=[]
     for i in res2:
         confirmed_total.append(i['confirmed']['total'])
+        increase_confirmed.append(confirmed_total[-1]-confirmed_total[-2])
         reportDate.append(i['reportDate'])
         deaths_total.append(i['deaths']['total'])
-    date=parse(res['lastUpdate'][:19])  
+        increase_deaths.append(deaths_total[-1]-deaths_total[-2])
+        date=parse(res['lastUpdate'][:19])  
+    confirmed_change=increase_confirmed[-1]
+    death_change=increase_deaths[-1]
     for i in res3:
-        print(i['name'])
         country_list.append(i['name'])
 
     context={
@@ -31,21 +42,24 @@ def home(request):
         'deathCount':res['deaths']['value'],
         'lastUpdate':date,
         'confirmed_total':confirmed_total,
+        'increase_confirmed':increase_confirmed,
+        'increase_deaths':increase_deaths,
         'deaths_total':deaths_total,
         'reportDate':reportDate,
+        'confirmed_change':increase_confirmed[-1],  # Showing increase in confirmed cases
+        'death_change': increase_deaths[-1],
+        'increase_recovered':increase_recovered[-1],  # Showing increase in death cases
         'country_list':country_list,
+        'change':5,
         'bar':False,
     }
     return render(request,'app/index.html',context)
 def countryInfo(request):
     if request.method=='POST':
         country=request.POST['c_list']
-        print(country)
         country_url=('https://covid19.mathdro.id/api/countries/'+country)
         countrylist_url='https://covid19.mathdro.id/api/countries'
-        print(country_url)
         res4=requests.get(country_url).json()
-        print(res4)
         res3=requests.get(countrylist_url).json()['countries']
         country_list=[]
         for i in res3:
@@ -62,6 +76,7 @@ def countryInfo(request):
             'bar':True,
             'lastUpdate':parse(requests.get('https://covid19.mathdro.id/api').json()['lastUpdate'][:19]),
             'country_list':country_list,
+            'change':0,
         }
         return render(request,'app/index.html',context)
     else:
